abcbf4dad30adf8da52455fc0efa09da1a111789
SravanMuddamsetty <76273242+SravanMuddamsetty@users.noreply.github.com>
2026-05-14 22:19:57 -0400

    with route navigation - init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/components/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '*',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    // loadChildren: () => import('./features/dashboard/dashboard.module').then(m = > m.DashboardModule)
+  },
+  // {
+  //   path: 'students',
+  // },
+  // {
+  //   path: 'teachers',
+  // },
+  // {
+  //   path: 'classes',
+  // },
+  // {
+  //   path: 'subjects',
+  // },
+];
--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  constructor(public router: Router) {}
 
+  OnClickNavigate() {
+    this.router.navigate(['/dashboard']);
+  }
 }
