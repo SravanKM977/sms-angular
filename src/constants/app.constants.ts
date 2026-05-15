51bee75c479b5493a87a68503695c5aa55c636c1
SravanMuddamsetty <76273242+SravanMuddamsetty@users.noreply.github.com>
2026-05-14 22:03:26 -0400

    header, footer, main commit

---
+++ src/constants/app.constants.ts
@@ -0,0 +1,9 @@
+export const application = {
+  paths: {
+    dashboard: 'dashboard',
+    students: 'students',
+    teachers: 'teachers',
+    classes: 'classes',
+    subjects: 'subjects',
+  },
+};
